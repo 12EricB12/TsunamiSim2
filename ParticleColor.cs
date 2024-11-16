aa04f17b3d21cafc45a548a78307582f0a182df3
t-moses <t.m.sug@bell.net>
2024-11-16 16:42:46 -0500

    Update ParticleColor.cs
    Add component to Base_Particle and change scale factor to around 10.

--- ParticleColor.cs
+++ ParticleColor.cs
@@ -4,21 +4,20 @@ public class ParticleColor : MonoBehaviour
 {
     private Rigidbody2D rb;
     SpriteRenderer sr;
+
+    // choose higher scale factor for lower velocities so full gradient is shown
     public int scaleFactor;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.linearVelocityX = 10;
         sr = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // colour of particle becomes more red at a higher velocity
         Vector2 vel = rb.linearVelocity;
-        print(vel.magnitude);
         float scaledVel = vel.magnitude * scaleFactor / 256;
         sr.color = new Color(scaledVel, 0f, 1 - scaledVel, 1);
     }
